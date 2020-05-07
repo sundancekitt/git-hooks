578e3585d19de810d43def21e839b1b5435290bb
Niels <niels.majer@posteo.de>
2020-05-07 09:58:16 +0200

    Add prepare commit mesage hook.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # git-hooks
+Collection of git hooks. Currently there is only one hook here:
+
+## Prepare commit message hook
+This hook automatically adds the branch name to the commit message.
+If not present in the commit message already, the current branch name is prepended to the commit message.
+On develop branch the commit message is left untouched.
+
+*Note*: This is only useful when the branch name starts with the issue key optionally followed by any string sequence starting with a non-numeric character (BRAHMAN-123 or BRAHMAN-123.2... or BRAHMAN-123-amendements...).
+
+### Requires
+* Rust/cargo
+
+To install `cargo` run the following command:
+```
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+```
+
+Cargo does not work on Windows 7 out-of-the-box because of outdated default settings in Windows 7. The [update](https://support.microsoft.com/en-us/help/3140245/update-to-enable-tls-1-1-and-tls-1-2-as-a-default-secure-protocols-in) fixes this (there is a `download` button).
+
+On Ubuntu you may need to install Openssl C headers:
+```
+sudo apt install libssl-dev
+```
+
+### Build instructions
+Run:
+
+```
+cargo build --release
+```
+
+Copy the binary file under `target/release/prepare-commit-msg` to the desired git hooks directory (under `.git/hooks` in the respective git directory).
+
+Never write the issue key into the commit message again :)
+
---
+++ src/bin/prepare-commit-msg.rs
@@ -0,0 +1,80 @@
+extern crate git2;
+extern crate regex;
+
+use git2::Repository;
+use regex::Regex;
+
+use std::fs::{File, read_to_string};
+use std::io::Write;
+use std::process;
+use std::env;
+
+fn main() {
+    let commit_filename = match env::args().nth(1) {
+        Some(a) => a,
+        None => {
+            eprintln!("Commit file was not provided");
+            process::exit(1);
+        }
+    };
+
+    // The commit source will be filled with labels like 'merge', 'amend'
+    // or 'message' to say how you got to this point.
+    let commit_source = env::args().nth(2);
+
+    let current_branch = get_current_branch().unwrap_or_else(|err| {
+        eprintln!("Failed to find current branch. {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = prepend_branch_name(current_branch, commit_filename,
+                                        commit_source) {
+        eprintln!("Failed to prepend message. {}", e);
+        process::exit(1);
+    }
+}
+
+fn get_current_branch() -> Result<String, git2::Error> {
+    let git_repo = Repository::discover("./")?;
+    let head = git_repo.head()?;
+    let head_name = head.shorthand();
+    match head_name {
+        Some(name) => Ok(name.to_string()),
+        None => Err(git2::Error::from_str("No branch name found"))
+    }
+}
+
+fn prepend_branch_name(branch_name: String,
+                       commit_filename: String,
+                       source: Option<String>) -> Result<(), std::io::Error> {
+    if branch_name == "develop".to_string() {
+        println!("Branch is develop. Not adding branch name to commit message.");
+        return Ok(());
+    }
+
+    let current_message = read_to_string(&commit_filename)?;
+
+    let re = Regex::new(r"^\s*(\w+-\d+)").unwrap();
+    if !re.is_match(&current_message) {
+        match re.captures(&branch_name) {
+            None => eprintln!("Could not identify valid id in {}", branch_name),
+            Some(m) => {
+                let branch_id = m.get(1).map_or("", |m| m.as_str());
+                write_file(&commit_filename, &current_message, branch_id, source == None)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn write_file(commit_filename: &String,
+              current_message: &String,
+              branch_id: &str, newline: bool) -> Result<(), std::io::Error> {
+    let mut commit_file = File::create(commit_filename)?;
+    if newline {
+        writeln!(commit_file, "{} --", branch_id)?;
+    } else {
+        write!(commit_file, "{} -- ", branch_id)?;
+    }
+    write!(commit_file, "{}", current_message)
+}
